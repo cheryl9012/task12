9cb9606c1e069d451e56730e8ab6e02ecd31a01d
cheryl9012 <161365360+cheryl9012@users.noreply.github.com>
2024-03-23 20:50:42 +0500

    Add files via upload
    In this task, friends names have been stored in Array

---
+++ Q12.ts
@@ -0,0 +1,11 @@
+// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+
+let personsname: string[]=["Jennifer","Aster","Anum","Sheryl"];
+
+let message: string= "How are you doing?"
+
+console.log(personsname[0] + " "+ message);
+console.log(personsname[1] + " "+ message);
+console.log(personsname[2] + " "+ message);
+console.log(personsname[3] + " "+ message);
